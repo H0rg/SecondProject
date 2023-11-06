0f026a263ce3fea648cbdc041141dcc6730dcb26
Horg <horgovi4@gmail.com>
2023-11-06 19:50:17 +0200

    Initial commit

---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private CharacterController _characterController;
+    public float moveSpeed = 6.0f;
+    public float rotSpeed = 15.0f;
+
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _vertSpeed = minFall;
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        if (horInput != 0 || vertInput != 0)
+        {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
+        }
+
+        if (_characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+                _vertSpeed = jumpSpeed;
+            else
+                _vertSpeed = minFall;
+            
+        }
+        else
+        {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocity)
+                _vertSpeed = terminalVelocity;
+        }
+        movement.y = _vertSpeed;
+
+        movement *= Time.deltaTime;
+        _characterController.Move(movement);
+    }
+}
