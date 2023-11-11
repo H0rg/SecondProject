c8bdf19b63d1e0147a4a03ec49172d45ab1a131c
Horg <horgovi4@gmail.com>
2023-11-11 20:56:34 +0200

    Rebuild whole project

--- Assets/Scripts/FirstLevel/Managers/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameEvent
-{
-    public const string HEALTH_UPDATED = "HELATH_UPDATED";
-    
-}
